d342ac6e0daf3f98e4ef7779d4e22071ef4ce40e
Mox <moxalibudbud@gmail.com>
2020-10-26 02:42:51 -0700

    shopping cart observer

--- src/app/shared/service/cart.service.ts
+++ src/app/shared/service/cart.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observer, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+	private cartChangeSource = new Subject<any>();
+	public cart$ = this.cartChangeSource.asObservable();
+	public cartMetaData: any;
 
   constructor() { }
+
+  updateCart(): void {
+  	this.user = user;
+    this.cartChangeSource.next(this.user);
+  }
 }
