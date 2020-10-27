1baea722c9143e73a478c93d5c0e4bc34f5a8ca0
Mox <moxalibudbud@gmail.com>
2020-10-27 01:50:56 -0700

    Prepare checkout page

--- src/app/modules/checkout/checkout.component.ts
+++ src/app/modules/checkout/checkout.component.ts
@@ -13,3 +13,26 @@ export class CheckoutComponent implements OnInit {
   }
 
 }
+
+
+// {
+//     "items": [
+//         {
+//             "product": "5f9157519fff8f1463316605",
+//             "merchant": "5f9156ea18e6bd1448ea71db",
+//             "price": 10.12,
+//             "qty": 1
+//         },
+//         {
+//             "product": "5f9157699badf9146b551960",
+//             "merchant": "5f9273ef1e0f191c9e85fbe5",
+//             "price": 10.3,
+//             "qty": 5
+//         }
+//     ],
+//     "customerDetails": {
+//         "name": "Test",
+//         "shippingAddress": "Test",
+//         "contactNo": "Test"
+//     }
+// }
