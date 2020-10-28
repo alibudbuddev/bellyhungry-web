32215dcb5c30f9dbc1487d06ea342156df0ce50f
Mox <moxalibudbud@gmail.com>
2020-10-27 23:56:09 -0700

    order service

---
+++ src/app/shared/service/order.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '@environment/environment'
+
+interface CreateOrderInterface {
+	items: {product: string, merchant: string, price: number, qty: number }[];
+	customerDetails: {name: string, shippingAddress: string, contactNo: string}
+}
+
+@Injectable()
+export class OrderService {
+	private endpoint = `${environment.apiUrl}/orders`;
+	public user: undefined | object | boolean;
+  
+  constructor(private httpClient: HttpClient) { }
+
+  create(order: CreateOrderInterface):Observable<any> {
+    return this.httpClient.post<any>(`${this.endpoint}`, order);
+  }
+
+}
