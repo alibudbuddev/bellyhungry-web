2e94f3bffe6d92be1ded726028457b85a8f7aef2
Mox <moxalibudbud@gmail.com>
2020-10-27 23:56:30 -0700

    Prepare summary form

--- src/app/modules/checkout/summary/checkout-summary.component.ts
+++ src/app/modules/checkout/summary/checkout-summary.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import CartService from '@shared/service/cart.service';
 import { CustomerDeliveryDetails } from '@shared/class/shopping-cart';
+import { OrderService } from '@shared/service/order.service';
 
 @Component({
   selector: 'app-checkout-summary',
   templateUrl: './checkout-summary.component.html',
-  styleUrls: ['./checkout-summary.component.less']
+  styleUrls: ['./checkout-summary.component.less'],
+  providers: [OrderService]
 })
 export class CheckoutSummaryComponent implements OnInit {
 	public items: any[] = [];
   public customerDeliveryDetails: CustomerDeliveryDetails;
   public isLoading: boolean;
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private orderService: OrderService, private router: Router) {
     this.customerDeliveryDetails = cartService.cart.getCustomerDeliveryDetails();
   	this.items = cartService.cart.getItems();
   }
@@ -26,6 +29,25 @@ export class CheckoutSummaryComponent implements OnInit {
 
   placeOrder(): void {
   	this.isLoading = true;
+    const items = this.cartService.cart
+    const order = {
+      customerDetails: this.customerDeliveryDetails,
+      items: this.items.map(x => {
+        let item = {
+          product: x.product,
+          merchant: x.merchant._id,
+          price: x.price,
+          qty: x.qty
+        };
+        return item;
+      })
+    };
+    this.orderService.create(order)
+      .subscribe(res => {
+        this.cartService.init();
+        this.cartService.updatView();
+        this.router.navigate(['/order', res._id]);
+      });
   }
 
 }
