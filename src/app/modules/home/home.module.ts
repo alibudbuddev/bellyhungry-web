da0941d6f8894b6d4d221d7813a3bd42dc86eebb
Mox <moxalibudbud@gmail.com>
2020-11-04 05:59:59 -0800

    category card component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ElementsModule } from '@shared/modules';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HomeCategoryListComponent } from './components/category-list/home-category-list.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeCategoryListComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
